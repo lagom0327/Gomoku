877fd959e884fb682a7c554d2b23d791055be65b
lagom0327 <jz00z0019@gapp.nthu.edu.com>
2019-11-23 04:48:17 +0800

    Updates

---
+++ precache-manifest.318b27ac6f31b175a59bd47aec6c179e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acb9bc4622b320bbca231bfb5c59250b",
+    "url": "/Gomoku/index.html"
+  },
+  {
+    "revision": "ac6a10d4394b27f5a388",
+    "url": "/Gomoku/static/css/main.cc4db509.chunk.css"
+  },
+  {
+    "revision": "8f098bf4f671f6026044",
+    "url": "/Gomoku/static/js/2.fa3469d1.chunk.js"
+  },
+  {
+    "revision": "ac6a10d4394b27f5a388",
+    "url": "/Gomoku/static/js/main.d71dd8b6.chunk.js"
+  },
+  {
+    "revision": "8ea82ac243e0e22254d8",
+    "url": "/Gomoku/static/js/runtime-main.baa5b391.js"
+  }
+]);
